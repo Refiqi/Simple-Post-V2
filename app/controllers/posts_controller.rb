9d851a2b68e889e3ed55a03a011e2d9adcf3404d
RefiqiMF <refyfadhila96@gmail.com>
2018-11-14 09:28:06 +0700

    Added CommentLine

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,54 +1,51 @@
 class PostsController < ApplicationController
-	def index
-		@post = Post.all
+  #Memperlihatkan Semua post di View Index
+  def index
+	@post = Post.all
+  end
+  #Memperlihatkan Post Melalui id nya
+  def show
+	@post = Post.find(params[:id])
+  end
+  #Membuat Post baru
+  def new
+	@post = Post.new
+  end
+  #Membuat Post baru
+  def create
+	@post = Post.new(post_params)
+	if @post.save
+       redirect_to @post
+	else
+	   render 'new'
 	end
-
-	def show
-		@post = Post.find(params[:id])
-		
-	end
-
-	def new
-		@post = Post.new
-	end
-
-	def create
-		#render plain: params[:post].inspect
-		@post = Post.new(post_params)
-		if @post.save
-
-		redirect_to @post
-		else
-		render 'new'
-		end
-	end 
-
-	def edit
-		@post = Post.find(params[:id]) 
-	end
-
-	def update
-		@post = Post.find(params[:id]) 
-
-		if (@post.update(post_params))
-			redirect_to @post
-		else
-		render 'edit'
-		end
-	end
-
-	def destroy
-		@post = Post.find(params[:id])
-		@post.destroy
-
-		redirect_to posts_path
-	end
+  end 
+  #Mengedit Post Sesuai id nya
+  def edit
+	@post = Post.find(params[:id]) 
+  end
+  #Mengupdate Post di Database yang sudah di ubah di Edit
+  def update
+	@post = Post.find(params[:id]) 
+      
+	  if (@post.update(post_params))
+		 redirect_to @post
+	  else
+	     render 'edit'
+	  end
+  end
+  #Menghapus Post sesuai Id nya
+  def destroy
+	@post = Post.find(params[:id])
+	@post.destroy
+    redirect_to posts_path
+  end
 	
-	private 
-
-	def post_params
-	params.require(:post).permit(:title,:body)
-	end
+ private 
+  #Memberi Parameter untuk form nya 
+  def post_params
+    params.require(:post).permit(:title,:body)
+  end
 
 end
 
